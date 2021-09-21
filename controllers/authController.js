398fc210551eef5dffbbd4f49799d4a22d86afc4
Asim Shrestha <asimshrestha608@gmail.com>
2021-09-21 07:59:48 +0545

    Create: basic auth controllers for login & signup

---
+++ controllers/authController.js
@@ -0,0 +1,42 @@
+/*
+Route: /signup
+Method: GET
+Access: Public
+*/
+const signup_get = (req, res) => {
+  res.render("signup");
+};
+
+/*
+Route: /signup
+Method: POST
+Access: Public
+*/
+const signup_post = (req, res) => {
+  res.send("New user created");
+};
+
+/*
+Route: /login
+Method: GET
+Access: Public
+*/
+const login_get = (req, res) => {
+  res.render("login");
+};
+
+/*
+Route: /login
+Method: POST
+Access: Public
+*/
+const login_post = (req, res) => {
+  res.send("User logged in");
+};
+
+module.exports = {
+  signup_get,
+  signup_post,
+  login_get,
+  login_post,
+};
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,29 +1,33 @@
 const router = require("express").Router();
 
+const authController = require("../controllers/authController");
+
 /*
 Route: /signup
 Method: GET
 Access: Public
 */
-router.get("/signup", () => {});
+router.get("/signup", authController.signup_get);
 
 /*
 Route: /signup
 Method: POST
 Access: Public
 */
-router.post("/signup", () => {});
+router.post("/signup", authController.signup_post);
 
 /*
 Route: /login
 Method: GET
 Access: Public
 */
-router.get("/login", () => {});
+router.get("/login", authController.login_get);
 
 /*
 Route: /login
 Method: POST
 Access: Public
 */
-router.post("/login", () => {});
+router.post("/login", authController.login_post);
+
+module.export = router;
