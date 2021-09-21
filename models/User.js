a44e4515f17ca0a12b7000f65327a5b08acb7d1d
Asim Shrestha <asimshrestha608@gmail.com>
2021-09-21 09:07:10 +0545

    Validation: check valid email & add custom vaidation error message

--- models/User.js
+++ models/User.js
@@ -1,19 +1,22 @@
 const mongoose = require("mongoose");
+const { isEmail } = require("validator");
+
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
   email: {
     type: String,
-    required: true,
+    required: [true, "Please enter email"],
     unique: true,
     lowercase: true,
     trim: true,
+    validate: [isEmail, "Please enter a valid email"],
   },
   password: {
     type: String,
-    required: true,
+    required: [true, "Please enter password"],
     trim: true,
-    minlength: 8,
+    minlength: [8, "Minimum password length is 8 characters"],
   },
 });
 
