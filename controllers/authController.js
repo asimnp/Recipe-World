092eb2497da014663e2c41dab9d1c8dc594733cf
Asim Shrestha <asimshrestha608@gmail.com>
2021-09-21 09:08:22 +0545

    Validation: add user-friendly error message for signup

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,3 +1,24 @@
+const User = require("../models/User");
+
+// Handle errors
+const handleErrors = (err) => {
+  let errors = { email: "", password: "" };
+
+  // duplicate email error code
+  if (err.code === 11000) {
+    errors.email = "Email already taken";
+    return errors;
+  }
+
+  // validation errors
+  if (err.message.includes("user validation failed")) {
+    Object.values(err.errors).forEach(({ properties }) => {
+      errors[properties.path] = properties.message;
+    });
+  }
+  return errors;
+};
+
 /*
 Route: /signup
 Method: GET
@@ -12,9 +33,16 @@ Route: /signup
 Method: POST
 Access: Public
 */
-const signup_post = (req, res) => {
+const signup_post = async (req, res) => {
   const { email, password } = req.body;
-  res.send("New user created");
+
+  try {
+    const user = await User.create({ email, password });
+    res.status(201).json(user);
+  } catch (err) {
+    const errors = handleErrors(err);
+    res.status(400).json({ errors });
+  }
 };
 
 /*
