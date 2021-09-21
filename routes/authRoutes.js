17e6ce93fda32954fdd5f882ca972b4c55259cde
Asim Shrestha <asimshrestha608@gmail.com>
2021-09-21 07:50:00 +0545

    Create: routes for signup & login

---
+++ routes/authRoutes.js
@@ -0,0 +1,29 @@
+const router = require("express").Router();
+
+/*
+Route: /signup
+Method: GET
+Access: Public
+*/
+router.get("/signup", () => {});
+
+/*
+Route: /signup
+Method: POST
+Access: Public
+*/
+router.post("/signup", () => {});
+
+/*
+Route: /login
+Method: GET
+Access: Public
+*/
+router.get("/login", () => {});
+
+/*
+Route: /login
+Method: POST
+Access: Public
+*/
+router.post("/login", () => {});
