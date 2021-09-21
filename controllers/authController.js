36b586459735c4efacc1bf8dd4403131cb9d7351
Asim Shrestha <asimshrestha608@gmail.com>
2021-09-21 13:09:51 +0545

    Feat: user can login

--- controllers/authController.js
+++ controllers/authController.js
@@ -75,9 +75,17 @@ Route: /login
 Method: POST
 Access: Public
 */
-const login_post = (req, res) => {
+const login_post = async (req, res) => {
   const { email, password } = req.body;
-  res.send("User logged in");
+
+  try {
+    const user = await User.login(email, password);
+    const token = createToken(user._id);
+    res.cookie("jwt", token, { httpOnly: true, maxAge: maxAge * 1000 });
+    res.status(200).json({ user: user._id });
+  } catch (err) {
+    res.status(400).json({ errors: err.message });
+  }
 };
 
 module.exports = {
