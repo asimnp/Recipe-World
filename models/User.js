2228466959b31e791efcb387bf85d26b29be96ac
Asim Shrestha <asimshrestha608@gmail.com>
2021-09-21 08:29:36 +0545

    Create: user model

---
+++ models/User.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 8,
+  },
+});
+
+const User = mongoose.model("user", userSchema);
+
+module.exports = UserModel;
